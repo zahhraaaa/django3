02fb090631be0a8de76d3a4aa492ceeb6a267dd5
zahhraaaa <zahraa.amini.79@gmail.com>
2021-06-18 10:27:32 +0430

    tag2

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
          name='post_detail'),
     path('<int:post_id>/share/',
          post_share, name='post_share'),
+    path('tag/<slug:tag_slug>/',post_list, name='post_list_by_tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,14 @@ from .models import Post,Comment
 from django.core.mail import send_mail
 from .forms import EmailPostForm,CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
 
-
-def post_list(request):
+def post_list(request,tag_slug=None):
     object_list = Post.objects.filter(status='published')
+    tag = None
+    if tag_slug:
+      tag = get_object_or_404(Tag, slug=tag_slug)
+      object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)  # 2 posts in each page
     page = request.GET.get('page')
     try:
@@ -19,7 +23,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
